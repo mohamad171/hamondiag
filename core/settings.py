749931be1e528d1180f82b0f9f03e51a64980092
Mohamad <mohamadmohamadi249@gmail.com>
2024-02-16 11:21:53 +0330

    allow hosts

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+CORS_ALLOW_ALL_ORIGINS = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-btxg!t2##)4%!&-fl@&k17$s2ztop%s_ro4kzf#iivm6ucr&uo'
 
